8c73f1d81633236a39605c440d355de474c5fa5c
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-02-12 13:39:29 +0100

    Updated to cms 3

--- people/admin.py
+++ people/admin.py
@@ -1,22 +1,19 @@
 """Admin classes for the ``people`` app."""
 from django.contrib import admin
-from django.utils.translation import get_language
 from django.utils.translation import ugettext_lazy as _
 
-from simple_translation.admin import TranslationAdmin
-from simple_translation.utils import get_preferred_translation_from_lang
+from hvad.admin import TranslatableAdmin
 
 from . import models
 
 
-class NationalityAdmin(TranslationAdmin):
+class NationalityAdmin(TranslatableAdmin):
     """Admin for the ``Nationality`` model."""
-    list_display = ['name', 'languages']
+    list_display = ['get_name', 'all_translations']
 
-    def name(self, obj):
-        lang = get_language()
-        return get_preferred_translation_from_lang(obj, lang).name
-    name.short_description = _('Name')
+    def get_name(self, obj):
+        return obj.name
+    get_name.short_description = _('Name')
 
 
 class LinkAdmin(admin.ModelAdmin):
@@ -29,23 +26,22 @@ class LinkInline(admin.TabularInline):
     model = models.Link
 
 
-class LinkTypeAdmin(TranslationAdmin):
+class LinkTypeAdmin(TranslatableAdmin):
     """Admin for the ``LinkType`` model."""
-    list_display = ['name', 'ordering', 'languages', ]
+    list_display = ['get_name', 'ordering', 'all_translations', ]
 
-    def name(self, obj):
-        lang = get_language()
-        return get_preferred_translation_from_lang(obj, lang).name
-    name.short_description = _('Name')
+    def get_name(self, obj):
+        return obj.name
+    get_name.short_description = _('Name')
 
 
-class PersonAdmin(TranslationAdmin):
+class PersonAdmin(TranslatableAdmin):
     """Admin for the ``Person`` model."""
     inlines = [LinkInline, ]
     list_display = [
         'roman_first_name', 'roman_last_name', 'non_roman_first_name_link',
         'non_roman_last_name', 'chosen_name', 'gender', 'title', 'role',
-        'phone', 'email', 'ordering', 'languages', ]
+        'phone', 'email', 'ordering', 'all_translations', ]
 
     def non_roman_first_name_link(self, obj):
         return u'<a href="{0}/">{1}</a>'.format(
@@ -54,14 +50,13 @@ class PersonAdmin(TranslationAdmin):
     non_roman_first_name_link.short_description = "Non roman first name"
 
 
-class RoleAdmin(TranslationAdmin):
+class RoleAdmin(TranslatableAdmin):
     """Admin for the ``Role`` model."""
-    list_display = ['name', 'languages', ]
+    list_display = ['get_name', 'all_translations', ]
 
-    def name(self, obj):
-        lang = get_language()
-        return get_preferred_translation_from_lang(obj, lang).name
-    name.short_description = _('Name')
+    def get_name(self, obj):
+        return obj.name
+    get_name.short_description = _('Name')
 
 
 admin.site.register(models.Nationality, NationalityAdmin)
