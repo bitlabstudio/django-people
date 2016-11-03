d276d767787a0b9fd963fb42094326b3b242b27f
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2016-11-03 11:59:27 +0100

    fixed hvad admin

--- people/admin.py
+++ people/admin.py
@@ -10,6 +10,7 @@ from . import models
 class NationalityAdmin(TranslatableAdmin):
     """Admin for the ``Nationality`` model."""
     list_display = ['get_name', 'all_translations']
+    list_select_related = []
 
     def get_name(self, obj):
         return obj.name
@@ -29,6 +30,7 @@ class LinkInline(admin.TabularInline):
 class LinkTypeAdmin(TranslatableAdmin):
     """Admin for the ``LinkType`` model."""
     list_display = ['get_name', 'ordering', 'all_translations', ]
+    list_select_related = []
 
     def get_name(self, obj):
         return obj.name
@@ -42,6 +44,7 @@ class PersonAdmin(TranslatableAdmin):
         'roman_first_name', 'roman_last_name', 'non_roman_first_name_link',
         'non_roman_last_name', 'chosen_name', 'gender', 'title', 'role',
         'phone', 'email', 'ordering', 'all_translations', ]
+    list_select_related = []
 
     def non_roman_first_name_link(self, obj):
         return u'<a href="{0}/">{1}</a>'.format(
@@ -53,6 +56,7 @@ class PersonAdmin(TranslatableAdmin):
 class RoleAdmin(TranslatableAdmin):
     """Admin for the ``Role`` model."""
     list_display = ['get_name', 'all_translations', ]
+    list_select_related = []
 
     def get_name(self, obj):
         return obj.name
